2f88cb599ff1d60c8d60a065248a475123d8113e
Conrad6 <philllittle302@gmail.com>
2020-09-26 16:15:20 +0100

    second commit

---
+++ src/app/models/Student.ts
@@ -0,0 +1,8 @@
+export interface Student {
+  name: string;
+  studentId: string;
+  _class: string;
+  address: string;
+  dob: Date;
+  contactNumber: string;
+}
---
+++ src/app/student.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {of} from 'rxjs';
+import * as data from './students.json';
+import {Student} from './models/Student';
+
+@Injectable({
+  providedIn:'root'
+})
+export class StudentService {
+  constructor(){
+    (data['data'] as any[]).forEach(x => x.dob = Date.parse(x.dob));
+  }
+  getStudents(){
+    
+    return of(data['data'] as Student[]);
+  }
+}
